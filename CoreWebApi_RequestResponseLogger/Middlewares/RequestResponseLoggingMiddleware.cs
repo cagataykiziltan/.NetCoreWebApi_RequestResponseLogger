deb43a39faaee676f705921f23ba293afbb4de46
cagataykiziltan <45563744+cagataykiziltan@users.noreply.github.com>
2019-08-07 23:08:01 +0300

    first commit
    first commit

---
+++ CoreWebApi_RequestResponseLogger/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWebApi_RequestResponseLogger.Middlewares
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        protected readonly ILogger _logger;
+        public RequestResponseLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+            _logger = NLog.LogManager.GetCurrentClassLogger();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = await FormatRequest(context.Request);
+
+            _logger.Info(request);
+
+            var originalBodyStream = context.Response.Body;
+            
+            using (var responseBody = new MemoryStream())
+            {
+                context.Response.Body = responseBody;
+
+                await _next(context);
+
+                var response = await FormatResponse(context.Response);
+
+                _logger.Info(response);
+
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+
+            request.EnableRewind();
+
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            request.Body = body;
+
+            return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return $"{response.StatusCode}: {text}";
+        }
+    }
+}
---
+++ CoreWebApi_RequestResponseLogger/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace CoreWebApi_RequestResponseLogger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).ConfigureLogging((hostingContext, logging) =>
+            {    
+            })
+                .UseStartup<Startup>();
+    }
+}
